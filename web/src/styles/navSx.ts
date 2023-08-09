7a10d58402fd66ad43f80f7c06b1fcb962c020ce
evnwttn <evnwttn@gmail.com>
2023-08-09 20:14:29 -0230

    mobile push

--- web/src/styles/navSx.ts
+++ web/src/styles/navSx.ts
@@ -6,10 +6,22 @@ export const navSx = {
     ml: "2.9vw",
     justifyContent: "space-between",
     mb: "2.5vw",
+    "@media (max-width: 480px)": {
+      mt: "1vw",
+      width: "58vw",
+    },
   },
   iconButton: {
     width: "3.5vw",
     height: "3.5vw",
+    "@media (max-width: 480px)": {
+      height: "10vw",
+      width: "10vw",
+    },
+    "@media (min-width: 481px) and (max-width: 767px)": {
+      height: "5vw",
+      width: "5vw",
+    },
     "&:hover": {
       cursor: "pointer",
       opacity: "0.7",
